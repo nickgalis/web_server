2f88e4fc7f72a3d52e6f84b4cf9f99df2513bab3
Anamu Uenishi <123331064+uenishia26@users.noreply.github.com>
2024-04-23 17:58:18 -0400

    Add files via upload
    Work in progress

--- webserv.c
+++ webserv.c
@@ -1,30 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <netinet/in.h> //For internet domain address 
 #include <string.h>
 #include <unistd.h>
+#include <arpa/inet.h>
+/*
+Socket is created
+accept() checks if there are any client requests on the listening socket socketfd
+    If request exists, accept() will take the request and return new socket to communicate with client 
 
+
+*/
 
 int main(int argc, const char *argv[])
 {
     int portNum = atoi(argv[1]); //Receive port number  
     char buffer [256]; 
 
+    char method[25]; //GET, POST, PUT
+    char uri[100]; //The resource client wants to interact with 
+    char version[10]; //Version of http 
+
     struct sockaddr_in serverAddr; //Instance of sockaddr_in
     socklen_t lenServerAddr = sizeof(serverAddr); //Size of serverAddr
     
-    int socketfd = socket(AF_INET, SOCK_STREAM, 0); //Internet socket w/h file like attributes read and write (TCP)
+    int socketfd = socket(AF_INET, SOCK_STREAM, 0); //Internet socket w/h file like attributes read and write SockStream(TCP)
     if(socketfd < 0)
         perror("Fail to create socket"); 
     
-    //Binding socket to server "Esentially I will receive infomration here from client"
+    //CREATING SERVER OF TYPE IPv4 
     bzero(&serverAddr, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET; //IPv4 address 
-    serverAddr.sin_port = htons(portNum);  //Convert into uint 16 
-    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddr.sin_port = htons(portNum);  //IP Port 
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY); //IP Address
 
-    //Bind socket to address 
+    //Bind socket to IP address
     if(bind(socketfd, (struct sockaddr*)&serverAddr, lenServerAddr) < 0)
         perror("Fail to bind socket"); 
     printf("Socket sucesfful bound to server\n"); 
@@ -42,23 +53,27 @@ int main(int argc, const char *argv[])
             perror("Error on accept"); 
         
         printf("Connection accepted\n"); 
-
+        printf("Connected to Client at %s %d\n", inet_ntoa(cliAddr.sin_addr), ntohs(cliAddr.sin_port)); 
         //read from socket 
         int valread = read(clientfd, buffer, sizeof(buffer));
+
+
+
         if(valread < 0)
             perror("Error (read)"); 
+        
+        printf("%s", buffer);
         
         char response [] = "HTTP/1.1 200 OK\r\n"
                             "Content-Type: text/html; charset=UTF-8\r\n"
                             "Content-Length: 13\r\n"
                             "\r\n"
                             "Hello, World!";
+        
 
-        write(clientfd, response, strlen(response)-1); 
-   
-    
-
-
+        write(clientfd, response, strlen(response)-1);
+        sleep(10); 
+        close(clientfd);
 
     }
 
